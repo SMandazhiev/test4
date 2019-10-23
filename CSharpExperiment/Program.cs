dd0e2571e36d29fd179bc14e77c32171d129a05c
SMandazhiev <simeonm@kingslanduniversity.com>
2019-10-23 11:46:24 +0300

    CSharp HelloWorld project

---
+++ CSharpExperiment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpExperiment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
